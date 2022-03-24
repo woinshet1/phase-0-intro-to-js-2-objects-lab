d0d504b6443b11e3cc9f2ed3adbc8df25a07c3a6
Woinshet <yemedhnmk@gmail.com>
2022-03-24 14:43:34 -0400

    obj

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your solution in this file!
+const employee={
+    name:"Sam",
+    streeAddress: "11 Broadway"};
+  let newemployee = "...employee"
+  delete employee.name;
+  //=> true
+  console.log(employee);
+  //{ streeAddress: '11 Broadway' }
+ // Hint: hit control+c anytime to enter REPL.
